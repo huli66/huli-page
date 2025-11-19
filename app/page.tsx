b90f823a91efe2ea4f9286a59bb1936be4832a2c
jianjun.hu <jianjun.hu@sumscope.com>
2025-11-19 20:00:37 +0800

    feat: style change

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex w-full h-full min-h-screen items-center justify-center font-sans">
+    <div className="flex w-full h-full min-h-screen items-center justify-center">
       <div className="card card-link w-[50%] h-[50px]">
         <h1 className="text-2xl font-bold text-red-500">Hello World</h1>
+      </div>
+      <div className="w-[50%] h-[50px] hover-card">
+        通过这里联系我吧
       </div>
     </div>
   );
